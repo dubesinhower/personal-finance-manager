ae4296c4111e23dfdec464c272e0d46932174ad4
dubesinhower <dubesinhower@gmail.com>
2016-09-06 20:38:10 -0400

    Imports

--- client/app/accounts/accounts.component.ts
+++ client/app/accounts/accounts.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Account, Transaction, AccountService } from './shared';
 
 @Component ({
     selector: 'pfm-accounts',
     templateUrl: 'app/accounts/accounts.component.html'
 })
-export class AccountsComponent {
+export class AccountsComponent implements OnInit {
+    accounts: Account[];
 
+    constructor(private _accountService: AccountService) {
+
+    }
+
+    ngOnInit() {
+        
+    }
 }
